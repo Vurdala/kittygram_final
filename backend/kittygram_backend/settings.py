162691556120cbbbf03394f57075aaee126b6de5
Vurdala <saske0268@gmail.com>
2025-11-26 21:32:10 +0300

    final

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,9 +1,9 @@
 import os
-
 from pathlib import Path
+
 from dotenv import load_dotenv
-from rest_framework.pagination import PageNumberPagination
 from django.core.management.utils import get_random_secret_key
+from rest_framework.pagination import PageNumberPagination
 
 load_dotenv()
 
